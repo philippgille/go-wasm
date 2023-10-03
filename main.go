2f3e32d74ccf20d05870887b69bf7b92439b0609
Philipp Gillé <philipp.gille@gmail.com>
2023-10-03 16:16:38 +0200

    WIP

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"io"
+	"net/http"
+)
+
 func main() {
 	println("Hello World")
+}
+
+func MakeHTTPRequest() {
+	res, err := http.Get("https://httpbin.org/get")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
 }
